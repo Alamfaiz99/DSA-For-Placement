83c6c788cdb2afcdc38b106136080fb0915623e9
S M Faizan ALam <faizanalam986@gmail.com>
2023-01-11 00:23:37 +0530

    c7

--- Day05 Arrays in java/Array2D.java
+++ Day05 Arrays in java/Array2D.java
@@ -2,9 +2,9 @@ import java.lang.*;
 import java.util.Scanner;
 
 public class Array2D {
-//    Lets atlk about 2-D array in java
+//    Lets talk about 2-D array in java
 // 2-D array is array of 1-D array
-// matrix si an example of 2-D array
+// matrix is an example of 2-D array
 /*
       [[1 2 3],
       [4 5 6],
@@ -14,15 +14,15 @@ public class Array2D {
  */
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-      int[][] arr=new int[3][];
+        int[][] arr=new int[3][];
     //   int[3][] ar={
     //     {1,2,3},
     //     {4,5,6},
     //     {7,8,9}
     //   }
-      int[][] ar={{1,2,3},//0th index
-                {1,2},//istindex
-                {1,2,3,4}//2nd index
+      int[][] ar={{1,2,3},//0th row
+                {1,2},//ist row
+                {1,2,3,4}//2nd row
             };
       System.out.println(arr.length);//no of rows
       int[][] arr1=new int[3][4];
@@ -32,5 +32,13 @@ public class Array2D {
             arr1[i][j]=sc.nextInt();
          }
       }
+      // in oredr to print the array you can use this approach
+      for (int i = 0; i < arr1.length; i++) {
+         //for each col in every row
+          for (int j = 0; j < arr1[i].length; j++) {
+             System.out.print(arr1[i][j]);
+          }
+          System.out.println();
+       } 
    }   
 }
---
+++ Day05 Arrays in java/ArrayList01.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayList01 {
+  public static void main(String[] args) {
+    // Now we will talk about ArrayList
+    // arrayList is dynamic approach to allocate memory when we 
+    // dont know how much elements we have to store..
+    // Here memory is allocated during runtime..
+    Scanner sc=new Scanner(System.in);
+     ArrayList<Integer> list=new ArrayList<>(10);
+    //  <> you can give the datatype of integer
+    // initial value you can pass as input
+    // add data in arraylist
+    list.add(10);
+    list.add(20);
+    list.add(30);
+    list.add(29);
+    list.add(50);
+    list.add(80);
+    // update the value
+    list.set(1, 76);
+    System.out.println(list);   
+  for(int i=0;i<5;i++){
+     list.add(sc.nextInt());
+  }
+  for(int i=0;i<5;i++){
+    System.out.println(list.get(i));
+ }}
+}
+// size is filled internally
+// say array list fills by some extent
+// it will create new array list of doublesize 
+// old elements are copied in new array
+
